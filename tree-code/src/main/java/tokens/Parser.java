61884b326adc45cacdce7d6c42d1a0abd7e5c952
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-12-03 12:24:16 +0100

    Added more parser

--- tree-code/src/main/java/tokens/Parser.java
+++ tree-code/src/main/java/tokens/Parser.java
@@ -1,45 +1,100 @@
 package tokens;
-import java.io.InputStream;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Parser {
 
-	private Scanner scanner;
 	private ParentStatement root = new ProgramStatement();
+	private int index = 0;
+	private List<Token> input;
 
-	public Parser(InputStream reader) {
-		this.scanner = new Scanner(reader);
-
-		while (scanner.hasNextLine()) {
-			parseStmt(root);
-		}
+	public Parser(Reader reader) throws IOException, LexerException {
+		final L3Lexer lexer = new L3Lexer(reader);
+		lexer.lexe();
+		this.input = lexer.getTokens();
 	}
 
 	private void parseStmt(ParentStatement parent) {
-		if (!scanner.hasNextLine()) {
-			return;
-		}
-		final String line = scanner.nextLine().trim().strip().toLowerCase();
-		if (line.isEmpty()) {
-			return;
-		}
+		final Token t = consume(TokenType.move)
+	}
+	
+	private boolean hasNext() {
+		return index < input.size();
+	}
 
-		final String[] tokens = line.split("\\s+");
-		switch (tokens[0]) {
-		case "move":
-			parent.add(new MoveStatement(Integer.parseInt(tokens[1])));
-			break;
-		case "rotate":
-			parent.add(new RotateStatement(Integer.parseInt(tokens[1])));
-			break;
+	private boolean hasNext(int x) {
+		return index + x < input.size();
+	}
 
-		default:
-			if(Direction.ALL.contains(tokens[0].toUpperCase())) {
-				parent.add(new DirectionStatement(Enum.valueOf(Direction.class, tokens[0].toUpperCase())));
-			}
-			break;
+	private Token consume() throws ParserException {
+		return consume(1);
+	}
+
+	private Token consume(int i) throws ParserException {
+		end();
+		Token c = input.get(index);
+		index += i;
+		return c;
+	}
+
+	private Token consume(TokenType t) throws ParserException {
+		if (!hasNext())
+			throw new ParserException("Expected %s but got end of input at: " + peek(-1).getPosition() + "->" + peek().getPosition(), t);
+
+		if (peek(t)) {
+			return consume();
+		} else {
+			throw new ParserException(peek(), t);
 		}
+	}
+
+	private Token consume(TokenType... types) throws ParserException {
+		Token peek = peek();
+		if (Arrays.stream(types).filter(peek.getType()::matches).collect(Collectors.counting()) > 0)
+			return consume();
+		else
+			throw new ParserException(peek, types);
+	}
+
+	private void end() throws ParserException {
+		if (!hasNext())
+			throw new ParserException("Unexpected end of input.");
+	}
+
+	private Token peek() {
+		return peek(0);
+	}
+
+	private Token peek(int i) {
+		return input.get(index + i);
+	}
+
+	private boolean peek(TokenType type) {
+		if (!hasNext())
+			return false;
+		return peek().getType().matches(type);
+	}
+
+	private boolean peek(int x, TokenType type) {
+		if (!hasNext(x))
+			return false;
+		return peek(x).getType().matches(type);
+	}
+
+	private boolean peek(TokenType... types) {
+		if (!hasNext())
+			return false;
+		TokenType peek = peek().getType();
+		return Arrays.stream(types).map(peek::matches).collect(Collectors.reducing((a, b) -> a || b)).orElse(false);
+	}
+
+	private boolean peek(int x, TokenType... types) {
+		TokenType peek = peek(x).getType();
+		return Arrays.stream(types).map(peek::matches).collect(Collectors.reducing((a, b) -> a || b)).orElse(false);
 	}
 
 }
--- tree-code/src/main/java/tokens/TokenType.java
+++ tree-code/src/main/java/tokens/TokenType.java
@@ -3,35 +3,7 @@ package tokens;
 
 public enum TokenType {
 
-	LET("let"), FUN("fun"),
-
-	RETURN("return"),
-
-	IMPORT("import"), PACKAGE("package"), AS("as"),
-
-	CLASS("class"), INTERFACE("interface"), STRUCT("struct"), UNION("union"),
-
-	TYPE(), USER_TYPE(TYPE), PRIMITIVE_TYPE(TYPE),
-
-	INT(PRIMITIVE_TYPE, "int"),
-
-	INT_1(INT, "int1"), INT_8(INT, "int8"), INT_16(INT, "int16"), INT_32(INT, "int32"), INT_64(INT, "int64"), INT_128(INT, "int128"),
-
-	INT_8_S(INT, "int8s"), INT_16_S(INT, "int16s"), INT_32_S(INT, "int32s"), INT_64_S(INT, "int64s"), INT_128_S(INT, "int128s"),
-
-	BYTE(INT_8, "byte"), CHAR(BYTE, "char"), SHORT(INT_16, "short"), LONG(INT_64, "long"),
-
-	FLOAT(PRIMITIVE_TYPE, "float"),
-
-	FLOAT_32(FLOAT, "float32"), /* FLOAT_128(FLOAT, "float128"), */
-
-	DOUBLE(PRIMITIVE_TYPE, "double"),
-
-	BOOLEAN(PRIMITIVE_TYPE),
-
 	TRUE("true"), FALSE("false"),
-
-	VOID("void"), NEW("new"),
 
 	NUM_LIT(), DEC_NUM_LIT(NUM_LIT), HEX_NUM_LIT(NUM_LIT), BIN_NUM_LIT(NUM_LIT), CHAR_LIT(NUM_LIT),
 
@@ -49,8 +21,6 @@ public enum TokenType {
 
 	IF("if"), ELSE("else"),
 
-	STATIC("static"),
-
 	SWITCH("switch"), CASE("case"), DEFAULT("default"),
 
 	FOR("for"), WHILE("while"), BREAK("break"),
@@ -61,25 +31,18 @@ public enum TokenType {
 
 	ASSIGN(), STRICT_ASSIGN(ASSIGN, '='),
 
-	BIT_OR('|'), BIT_AND('&'), BIT_XOR('^'), BIT_NOT('~'),
-
-	BIT_OR_ASSIGN(ASSIGN, "|="), BIT_AND_ASSIGN(ASSIGN, "&="), BIT_XOR_ASSIGN(ASSIGN, "^="), BIT_NOT_ASSIGN(ASSIGN, "~="),
-
 	OR("||"), AND("&&"), NOT('!'), XOR("^^"),
 
-	HASH('#'),
-
 	PLUS('+'), MINUS('-'), MUL('*'), DIV('/'), MODULO('%'),
-
-	PLUS_PLUS("++"), MINUS_MINUS("--"),
-
-	PLUS_ASSIGN(ASSIGN, "+="), MINUS_ASSIGN(ASSIGN, "-="), MUL_ASSIGN(ASSIGN, "*="), DIV_ASSIGN(ASSIGN, "/="), MODULO_ASSIGN(ASSIGN, "%="),
 
 	EQUALS("=="), NOT_EQUALS("!="),
 
 	LESS('<'), LESS_EQUALS("<="),
 
-	GREATER('>'), GREATER_EQUALS(">=");
+	GREATER('>'), GREATER_EQUALS(">="),
+	
+	// CUSTOM
+	MOVE("move"), UP("up"), DOWN("down"), ROTATE("rotate");
 
 	private TokenType parent;
 	private boolean fixed = false;
